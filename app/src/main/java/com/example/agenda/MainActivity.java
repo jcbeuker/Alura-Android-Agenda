696148ea7d62e7b1fa5512838d1704257b86999b
jcbeuker <jcbeuker@yahoo.com.br>
2020-03-06 20:13:54 -0300

    Aula 3 - Entendendo a teoria básica da construção de layouts - FAB

--- app/src/main/java/com/example/agenda/MainActivity.java
+++ app/src/main/java/com/example/agenda/MainActivity.java
@@ -4,8 +4,6 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 
