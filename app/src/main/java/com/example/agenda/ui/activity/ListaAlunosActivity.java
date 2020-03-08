1adf40193f8469e05e3f815e02ed84e57aea02e2
jcbeuker <jcbeuker@yahoo.com.br>
2020-03-08 14:22:00 -0300

    Aula 4 - Refatorando a MainActivity

--- app/src/main/java/com/example/agenda/ui/activity/ListaAlunosActivity.java
+++ app/src/main/java/com/example/agenda/ui/activity/ListaAlunosActivity.java
@@ -1,4 +1,4 @@
-package com.example.agenda;
+package com.example.agenda.ui.activity;
 
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
@@ -7,20 +7,22 @@ import android.widget.ListView;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.agenda.R;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 
-public class MainActivity extends AppCompatActivity {
+public class ListaAlunosActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_lista_alunos);
         setTitle("Lista de alunos");
         List<String> alunos = new ArrayList<>(
                 Arrays.asList("Bernardo", "Caetano", "Thays", "Maria", "Jose"));
-              ListView listaDeAlunos =  findViewById(R.id.activity_main_lista_de_alunos);
+              ListView listaDeAlunos =  findViewById(R.id.activity_lista_alunos_listview);
               listaDeAlunos.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, alunos));
     }
 }
